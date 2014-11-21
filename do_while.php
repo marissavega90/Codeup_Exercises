4d7e972323ee05091c05855117f376271607a95c
Marissa <marissavega90@gmail.com>
2014-11-21 13:48:56 -0600

    do_while Exercise 2

--- do_while.php
+++ do_while.php
@@ -1,11 +1,16 @@
 <?php
  
-$a = 0;
+$a = 100;
 
 do {
     
     echo "$a\n";
     
-    $a += 2;
+    $a -= 5;
 
-} while ($a <= 100);
+} while ($a >= -10);
+
+//REMEMBER WHAT YOU DID! 
+//First I put "+= 5" which is WRONG
+//needed to put "-= 5" in order to make it "* -5"
+//then fixed "while" from "$a <= -10" to "$a >= -10" which works
