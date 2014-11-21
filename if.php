827689a35def48f941d0956d6770c93f70dda644
Marissa <marissavega90@gmail.com>
2014-11-21 10:22:28 -0600

    Added if-else-elseif statement exercises

--- if.php
+++ if.php
@@ -45,3 +45,46 @@ if ($b !== $c) {
 }
 
 ?>
+
+<?php
+
+$a = 5;
+$b = 10; 
+$c = '10';
+
+// Shorten the next 2 statements into an if/else
+if ($a < $b) {
+    // output the appropriate result
+    echo "$a is less than $b\n";
+} else {
+    // output the appropriate result
+    echo "$b is greater than $a\n";
+}
+
+// Shorten the next 2 statements into an if/else
+if ($b >= $c) {
+    // output the appropriate result
+    echo "$b is greater than or equal to $c\n";
+} else {
+    // output the appropriate result
+    echo "$b is less than or equal to $c\n";
+}
+
+// combine the next 4 conditionals into
+// one if/else/elseif block that checks in order for:
+// identical, equal, not identical, not equal
+if ($b == $c) {
+    // output the appropriate result
+    echo "$b is equal to $c\n";
+} elseif ($b === $c) {
+    // output the appropriate result
+    echo "$b is identical to $c\n";
+} elseif ($b != $c) {
+    // output the appropriate result
+    echo "$b is not equal to $c\n";
+} else {
+    // output the appropriate result
+    echo "$b is not identical to $c\n";
+}
+
+?>
