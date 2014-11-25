3143088705bc03e3e6a292ec950aec920e55b918
Marissa <marissavega90@gmail.com>
2014-11-25 10:54:48 -0600

    FizzBuzz with better, prettier formatting

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,5 +1,5 @@
-<?php
 
+<?php
 $num1 = 1;
 
 $num2 = 100;
@@ -7,25 +7,17 @@ $num2 = 100;
 for ($i = $num1; $i <= $num2; $i++) {
 
     if ($i % 3 == 0 && $i % 5 == 0) {
-
         echo "FizzBuzz\n";
-
     }
 
     elseif ($i % 3 == 0) {
-
         echo "Fizz\n";
-
     }
 
     elseif ($i % 5 == 0) {
-
-     echo "Buzz\n";
-    }   
-
+        echo "Buzz\n";
     
-
-    else 
-
+    } else {
         echo "$i\n";
+    }
 }
