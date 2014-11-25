2b6539605e2928874da8b61e5c998b82c2e2d069
Marissa <marissavega90@gmail.com>
2014-11-25 13:42:10 -0600

    Answered question 4 (with help)

--- foreach.php
+++ foreach.php
@@ -3,39 +3,27 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $value) {
-    if (is_scalar($value)) {
-        echo "$value is a float, integer, string, or boolean\n";
+
+    if (is_integer($value)) {
+        echo "$value\n";
+    
+    } elseif (is_string($value)) {
+        echo "$value\n";
+    
+    } elseif (is_null($value)) {
+        echo "$value\n";
+
+    } elseif (is_float($value)) {
+        echo "$value\n";
+    
+    } elseif (is_bool($value)) {
+        echo "$value\n";
+    
+    } elseif (is_array($value)) {
+        foreach($value as $detail) {
+            echo "Array $detail\n";
+        }
+    
     }
+
 }
-
-    //CODE FOR STEP 2.
-
-    // if (is_integer($value)) {
-    //     echo "$value is an integer\n";
-    
-    // } elseif (is_string($value)) {
-    //     echo "$value is a string\n";
-    
-    // } elseif (is_null($value)) {
-    //     echo "$value is null\n";
-
-    // } elseif (is_float($value)) {
-    //     echo "$value is a float\n";
-    
-    // } elseif (is_bool($value)) {
-    //     echo "$value is a boolean\n";
-    
-    // } elseif (is_array($value)) {
-    //     print_r($value);
-
-                //Thomas' code!
-
-        // foreach ($variable as $value) {
-        //     # code...
-        // }
-
-        // echo "$value is an array\n";
-    
-    // }
-
-// }
