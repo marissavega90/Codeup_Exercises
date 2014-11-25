c5f34f574d17427ae5924b712888125ee90e5915
Marissa <marissavega90@gmail.com>
2014-11-25 09:10:53 -0600

    Added error message if user types in words instead of numbers

--- for.php
+++ for.php
@@ -3,24 +3,57 @@
 echo "Please input starting number \n";
 
 if ($startnum = trim(fgets(STDIN))) {
+
+    if (is_numeric($startnum)) {
+
     echo "Starting number is $startnum \n";
     echo "Please input ending number \n";
+
+    } else {
+
+        echo "Not a number!";
+
+        exit(0);
+
+      }
+
 }
 
 if ($endnum = trim(fgets(STDIN))) {
-    echo "Ending number is $endnum \n";
+
+    if (is_numeric($endnum)) {
+        echo "Ending number is $endnum \n";
+    } else {
+
+        echo "Not a number!";
+
+        exit (0);
+      
+      } 
 }
 
 echo "Count by what increment? \n";
 
 if ($increment = trim(fgets(STDIN))) {
-    echo "Increment is: $increment\n";
-    echo "The numbers you have chosen are: \n";
+    if (is_numeric($increment)) {
+        
+        echo "Increment is: $increment\n";
 
-    for ($i = $startnum; $i <= $endnum; $i += $increment) {
+        } else {
+
+            echo "Not a number!";
+
+            exit (0);
+
+            }
+
+        echo "The numbers you have chosen are: \n";
+
+        for ($i = $startnum; $i <= $endnum; $i += $increment) {
 
         echo "$i\n";
 
+        }
     }
-}
+
 
