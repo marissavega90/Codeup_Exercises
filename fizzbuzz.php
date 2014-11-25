27ff6c00a5b07dfff4ee9a50cb614cd980d38a8c
Marissa <marissavega90@gmail.com>
2014-11-25 11:04:49 -0600

    FizzBuzz with the best formatting

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,17 +1,14 @@
 
 <?php
-$num1 = 1;
 
-$num2 = 100;
-
-for ($i = $num1; $i <= $num2; $i++) {
+for ($i = 1; $i <= 100; $i++) {
 
     if ($i % 3 == 0 && $i % 5 == 0) {
         echo "FizzBuzz\n";
 
     } elseif ($i % 3 == 0) {
         echo "Fizz\n";
-        
+
     } elseif ($i % 5 == 0) {
         echo "Buzz\n";
     
