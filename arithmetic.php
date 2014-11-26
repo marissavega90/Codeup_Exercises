3d092a3974b8421761e2f76305b9adb02ccd9a98
Marissa <marissavega90@gmail.com>
2014-11-26 13:34:19 -0600

    Completed question 1 and 2, added code that calls each function with real numbers

---
+++ arithmetic.php
@@ -0,0 +1,27 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b . PHP_EOL;
+}
+
+add(4, 3);
+
+function subtract($a, $b) {
+    echo $a - $b . PHP_EOL;
+}
+
+subtract(30, 21);
+
+function multiply($a, $b) {
+    echo $a * $b . PHP_EOL;
+}
+
+multiply(5, 5);
+
+function divide($a, $b) {
+    echo $a / $b . PHP_EOL;
+}
+
+divide(12, 3);
+
+?>
