ff68bfe8870b1799476115787167bf554439a781
Marissa <marissavega90@gmail.com>
2014-11-26 15:39:27 -0600

    Refactored error message into it's own function, improved error parameters

--- arithmetic.php
+++ arithmetic.php
@@ -6,10 +6,14 @@ $num1 = trim(fgets(STDIN));
 $num2 = trim(fgets(STDIN));
 
 
+function errorMsg() {
+    echo "Error!: Both arguments must be numbers!\n";
+}
+
 function add($a, $b) {
 
-    if (!is_numeric($a) && !is_numeric($b)) {
-        echo "ERROR!: Both arguments must be numbers!\n";
+    if (!is_numeric($a) || !is_numeric($b)) {
+        errorMsg();
         echo var_dump($a, $b);
 
     } else {
@@ -22,8 +26,8 @@ add($num1, $num2);
 
 function subtract($a, $b) {
 
-    if (!is_numeric($a) && !is_numeric($b)) {
-        echo "ERROR!: Both arguments must be numbers!\n";
+    if (!is_numeric($a) || !is_numeric($b)) {
+        errorMsg();
         echo var_dump($a, $b);
 
     } else {
@@ -35,8 +39,8 @@ subtract($num1, $num2);
 
 
 function multiply($a, $b) {
-    if (!is_numeric($a) && !is_numeric($b)) {
-        echo "ERROR!: Both arguments must be numbers!\n";
+    if (!is_numeric($a) || !is_numeric($b)) {
+        errorMsg();
         echo var_dump($a, $b);
     } else {
         echo $a * $b . PHP_EOL;
@@ -48,8 +52,8 @@ multiply($num1, $num2);
 
 function divide($a, $b) {
 
-    if (!is_numeric($a) && !is_numeric($b)) {
-        echo "ERROR!: Both arguments must be numbers!\n";
+    if (!is_numeric($a) || !is_numeric($b)) {
+        errorMsg();
         echo var_dump($a, $b);
 
     } elseif ($a == 0 || $b == 0) {
@@ -64,7 +68,7 @@ divide($num1, $num2);
 
 function modulus($a, $b) {
     if (!is_numeric($a) || !is_numeric($b)) {
-        echo "ERROR!: Both arguments must be numbers!\n";
+        errorMsg();
         echo var_dump($a, $b);
 
     } elseif ($a == 0 || $b == 0) {
